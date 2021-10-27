27d71c7f3a187199a8d2b0c9b6778875b2674606
IAmDylanDennison <71900262+IAmDylanDennison@users.noreply.github.com>
2021-10-27 17:56:46 -0400

    Co-authored-by: Sam Hill <SamH7798@users.noreply.github.com>

--- main.py
+++ main.py
@@ -9,33 +9,106 @@ import matplotlib.pyplot as plt
 with open('diabetes.csv','r') as file:
     reader = csv.DictReader(file)
 
+    outcomes = []
     count = 0
     ## positive attributes
     pos_preg = []
+    pos_glucose = []
+    pos_blood_pres = []
+    pos_skin_thickness = []
+    pos_insulin = []
+    pos_BMI = []
+    pos_pedigree = []
     pos_age = []
+    pos_outcomes = []
+    pos_cases = 0
     
      ## negative attributes
     neg_preg = []
+    neg_glucose = []
+    neg_blood_pres = []
+    neg_skin_thickness = []
+    neg_insulin = []
+    neg_BMI = []
+    neg_pedigree = []
     neg_age = []
+    neg_outcomes = []
+    neg_cases = 0
 
+    # outcomes
+    outcomes = [0,1]
     # reads each row in csv file
     for row in reader:
-        #collects the age and pregnancies of women who tested positive
+        #collects the attributes of women who tested positive
         if row['Outcome'] == '1':
             pos_preg.append(int(row['Pregnancies']))
+            pos_glucose.append(int(row['Glucose']))
+            pos_blood_pres.append(int(row['BloodPressure']))
+            pos_skin_thickness.append(int(row['SkinThickness']))
+            pos_insulin.append(int(row['Insulin']))
+            pos_BMI.append(float(row['BMI']))
+            pos_pedigree.append(float(row['DiabetesPedigreeFunction']))
             pos_age.append(int(row['Age']))
+            pos_outcomes.append(int (row['Outcome']))
 
-      #collects the age and pregnancies of women who tested negative
+            outcomes.append(int (row['Outcome']))
+            pos_cases += 1
+
+            
+            
+            
+
+      #collects the attributes of women who tested negative
         if row['Outcome'] == '0':
             neg_preg.append(int(row['Pregnancies']))
+            neg_glucose.append(int(row['Glucose']))
+            neg_blood_pres.append(int(row['BloodPressure']))
+            neg_skin_thickness.append(int(row['SkinThickness']))
+            neg_insulin.append(int(row['Insulin']))
+            neg_BMI.append(float(row['BMI']))
+            neg_pedigree.append(float(row['DiabetesPedigreeFunction']))
             neg_age.append(int(row['Age']))
-        if count == 80:
+            neg_outcomes.append(int(row['Outcome']))
+
+            outcomes.append(int (row['Outcome']))
+            neg_cases += 1
+
+        if count == 500:
             break
         count += 1
 
-print(pos_age)
+#print(pos_age)
 
-def plot():
+
+
+def overall_plot():
+
+    # creating a hisogram of all cases
+    plt.figure(figsize=(10,6))
+    plt.bar(outcomes, height=[100,200,300,400,500])
+    #plt.hist(outcomes,bins =2)
+
+    ## labeling and titling the graph
+    plt.title("Diabetics in Pregnant women")
+    plt.xlabel("Outcome")
+    plt.ylabel("Cases")
+    plt.legend(["Diabetics","No Diabetics"])
+
+    plt.xticks(outcomes, ['0','1'])
+    ## setting the axis values
+    #x = [0,1]
+   # y= [100,200,300,400,500]
+    
+    #plt.xticks(x,x)
+    #plt.yticks(y,y)
+
+ 
+    plt.show()
+    
+    
+
+
+def pregnancy_age_plot():
 
     ## creating the scatter plot
     plt.figure(figsize=(10,6))
@@ -58,8 +131,78 @@ def plot():
 
     #plt.plot(x, y)
     plt.show()
+    
+def pedigree_insulin_plot():
+    
+    ## creating the scatter plot
+    plt.figure(figsize=(10,6))
+    plt.scatter(pos_insulin, pos_pedigree, c='green')
+    plt.scatter(neg_age, neg_preg, c='red')
+    
+    ## labeling and titling the graph
+    plt.title("Diabetics in Pregnant women")
+    plt.xlabel("Age")
+    plt.ylabel("Pregnancies")
+    plt.legend(["Diabetics","No Diabetics"])
+    
+
+    ## setting the axis values
+    x = [20,30,40,50,60,70,80]
+    y= [0,3,6,9,12,15,17]
+    
+    plt.xticks(x,x)
+    plt.yticks(y,y)
+
+    #plt.plot(x, y)
+    plt.show()
   
- 
+def BMI_age_plot():
+    
+    ## creating the scatter plot
+    plt.figure(figsize=(10,6))
+    plt.scatter(pos_age, pos_BMI, c='green')
+    plt.scatter(neg_age, neg_BMI, c='red')
+    
+    ## labeling and titling the graph
+    plt.title("BMI in Pregnant women")
+    plt.xlabel("Age")
+    plt.ylabel("BMI")
+    plt.legend(["Diabetics","No Diabetics"])
+    
+
+    ## setting the axis values
+    x = [20,30,40,50,60,70,80]
+    y= [0,20,25,30,35,40,45,50,60]
+    
+    plt.xticks(x,x)
+    plt.yticks(y,y)
+
+    #plt.plot(x, y)
+    plt.show()
+
+def Pedigree_age_plot():
+    
+    ## creating the scatter plot
+    plt.figure(figsize=(10,6))
+    plt.scatter(pos_age, pos_pedigree, c='green')
+    plt.scatter(neg_age, neg_pedigree, c='red')
+    
+    ## labeling and titling the graph
+    plt.title("Pedigree in Pregnant women")
+    plt.xlabel("Age")
+    plt.ylabel("Pedigree")
+    plt.legend(["Diabetics","No Diabetics"])
+    
+
+    ## setting the axis values
+    x = [20,30,40,50,60,70,80]
+    y= [0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 1.00, 1.10, 1.20, 1.30, 1.40, 1.50, 1.60, 1.70, 1.80, 1.90, 2.00]
+    
+    plt.xticks(x,x)
+    plt.yticks(y,y)
+
+    #plt.plot(x, y)
+    plt.show()
 
 # The main tkinter window
 window = Tk()
@@ -69,13 +212,23 @@ window.title('Diabetics in Pregnant women')
   
 # setting the dimensions of 
 # the main window
-window.geometry("500x500")
+window.geometry("1000x1000")
   
 # button that displays the plot
-plot_button = Button(master = window, command = plot, height = 2,  width = 10, text = "Plot")
-# place the button
+
+#place the buttons
+pregnancy_age_plot_button = Button(master = window, command = pregnancy_age_plot, height = 2,  width = 25, font=("Courier", 8), text = "Plot Pregnancy & Age")
+BMI_age_plot_button = Button(master = window, command = BMI_age_plot, height = 2,  width = 25, font=("Courier", 8), text = "Plot BMI & Age")
+outcome_button = Button(master = window, command = overall_plot, height = 2,  width = 25, font=("Courier", 8), text = "Outcome Plot")
+Pedigree_age_button = Button(master = window, command = Pedigree_age_plot, height = 2,  width = 25, font=("Courier", 8), text = "Predigree & Age Plot")
+
 # into the window
-plot_button.pack()
+outcome_button.pack()
+pregnancy_age_plot_button.pack()
+BMI_age_plot_button.pack()
+Pedigree_age_button.pack()
+
+
   
 # run the gui
 window.mainloop()
